bc9e077fec6ba925f625c87ebd74e47fd8210b30
mpavle1 <102835309+mpavle1@users.noreply.github.com>
2024-03-19 00:29:07 +0100

    Implementation
    Implemented:
    - Window display on start
    - Generating fixed amount of boids
    - Boids move, and stray away from walls

---
+++ README.md
@@ -0,0 +1,13 @@
+# Boids now in rust!
+
+## Introduction
+
+This is a rust based implementation of the [artificial life](https://en.wikipedia.org/wiki/Boids) program of the same name.
+
+## How to run
+
+Just run the following command, and a window with the simulation will appear
+
+```rust
+cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use std::f64::consts::{FRAC_PI_2, PI};
+
+use piston_window::math::Vec2d;
+use piston_window::*;
+use piston_window::{PistonWindow, WindowSettings};
+
+use rand::*;
+
+const SCREEN_WIDTH: u32 = 640;
+const SCREEN_HEIGHT: u32 = 480;
+
+const SCREEN_BUFFER: f64 = 15.0;
+
+#[derive(Copy, Clone)]
+struct Boid {
+    x: f64,
+    y: f64,
+    direction: f64, // Privremeno dok ne smislim kako cu ovo da pratim
+    velocity: f64,
+    color: [f32; 4],
+}
+
+fn rotate_point(point: Vec2d, angle_rad: f64, d: f64) -> Vec2d {
+    let cos_theta = angle_rad.cos();
+    let sin_theta = angle_rad.sin();
+
+    let new_x = point[0] - d * cos_theta;
+    let new_y = point[1] - d * sin_theta;
+
+    [new_x, new_y]
+}
+
+impl Boid {
+    fn new() -> Self {
+        let mut random = rand::thread_rng();
+        let two_pi = 2.0 * PI;
+
+        Self {
+            x: random.gen_range(100..=(SCREEN_WIDTH - 100)) as f64,
+            y: random.gen_range(100..=(SCREEN_HEIGHT - 100)) as f64,
+            velocity: 1.0,
+            direction: PI * random.gen_range(0.0..=two_pi),
+            color: [
+                random.gen_range(0.0..=1.0),
+                random.gen_range(0.0..=1.0),
+                random.gen_range(0.0..=1.0),
+                1.0,
+            ],
+        }
+    }
+
+    fn draw(&self, ctx: &Context, g: &mut G2d) {
+        let x = self.x;
+        let y = self.y;
+        let angle = self.direction;
+
+        let angle_rad = angle;
+
+        let a = rotate_point([x, y], FRAC_PI_2 + angle_rad, 5.0);
+        let b = rotate_point([x, y], FRAC_PI_2 + angle_rad, -5.0);
+        let c = rotate_point([x, y], angle_rad, 15.0);
+
+        polygon(self.color, &[a, b, c], ctx.transform, g);
+    }
+
+    fn update(&mut self) -> () {
+        let mut random = rand::thread_rng();
+
+        let [new_x, new_y] = rotate_point([self.x, self.y], self.direction, self.velocity);
+
+        if new_x < SCREEN_BUFFER {
+            self.direction = random.gen_range(PI * 0.75..=PI * 1.25);
+        } else if new_y < SCREEN_BUFFER {
+            self.direction = random.gen_range(PI * 1.25..=PI * 1.75);
+        } else if new_x > (SCREEN_WIDTH as f64 - SCREEN_BUFFER) {
+            self.direction = random.gen_range(PI * 1.75..=PI * 2.25)
+        } else if new_y > (SCREEN_HEIGHT as f64 - SCREEN_BUFFER) {
+            self.direction = random.gen_range(PI * 0.25..=PI * 0.75)
+        }
+
+        self.x = new_x;
+        self.y = new_y;
+    }
+}
+
+fn main() {
+    let mut window: PistonWindow =
+        WindowSettings::new("Boids... now in rust", [SCREEN_WIDTH, SCREEN_HEIGHT])
+            .resizable(false)
+            .build()
+            .unwrap();
+
+    let mut boids: Vec<Boid> = Vec::new();
+
+    for _ in 0..=20 {
+        boids.push(Boid::new());
+    }
+
+    // dodati proveri za delta time, tako da imamo konstantan FPS
+
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |ctx, g, _| {
+            clear([0.0, 0.0, 0.0, 1.0], g);
+
+            boids.iter_mut().for_each(|b| {
+                b.update();
+                b.draw(&ctx, g);
+            });
+        });
+    }
+}
