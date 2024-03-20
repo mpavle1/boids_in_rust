d35a53598756f4587767d4d17985b81e290b2b64
mpavle1 <102835309+mpavle1@users.noreply.github.com>
2024-03-20 19:04:35 +0100

    main cleanup

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,25 @@ const SCREEN_HEIGHT: u32 = 480;
 const SCREEN_BUFFER: f64 = 15.0;
 
 #[derive(Copy, Clone)]
-struct Boid {
+struct Point {
     x: f64,
     y: f64,
+}
+
+#[derive(Copy, Clone)]
+struct Boid {
+    point: Point,
     direction: f64, // Privremeno dok ne smislim kako cu ovo da pratim
     velocity: f64,
     color: [f32; 4],
 }
 
-fn rotate_point(point: Vec2d, angle_rad: f64, d: f64) -> Vec2d {
+fn rotate_point(point: Point, angle_rad: f64, d: f64) -> Vec2d {
     let cos_theta = angle_rad.cos();
     let sin_theta = angle_rad.sin();
 
-    let new_x = point[0] - d * cos_theta;
-    let new_y = point[1] - d * sin_theta;
+    let new_x = point.x - d * cos_theta;
+    let new_y = point.y - d * sin_theta;
 
     [new_x, new_y]
 }
@@ -33,13 +38,14 @@ fn rotate_point(point: Vec2d, angle_rad: f64, d: f64) -> Vec2d {
 impl Boid {
     fn new() -> Self {
         let mut random = rand::thread_rng();
-        let two_pi = 2.0 * PI;
 
         Self {
-            x: random.gen_range(100..=(SCREEN_WIDTH - 100)) as f64,
-            y: random.gen_range(100..=(SCREEN_HEIGHT - 100)) as f64,
+            point: Point {
+                x: random.gen_range(100..=(SCREEN_WIDTH - 100)) as f64,
+                y: random.gen_range(100..=(SCREEN_HEIGHT - 100)) as f64,
+            },
             velocity: 1.0,
-            direction: PI * random.gen_range(0.0..=two_pi),
+            direction: PI * random.gen_range(0.0..=(2.0 * PI)),
             color: [
                 random.gen_range(0.0..=1.0),
                 random.gen_range(0.0..=1.0),
@@ -50,15 +56,13 @@ impl Boid {
     }
 
     fn draw(&self, ctx: &Context, g: &mut G2d) {
-        let x = self.x;
-        let y = self.y;
         let angle = self.direction;
 
         let angle_rad = angle;
 
-        let a = rotate_point([x, y], FRAC_PI_2 + angle_rad, 5.0);
-        let b = rotate_point([x, y], FRAC_PI_2 + angle_rad, -5.0);
-        let c = rotate_point([x, y], angle_rad, 15.0);
+        let a = rotate_point(self.point, FRAC_PI_2 + angle_rad, 5.0);
+        let b = rotate_point(self.point, FRAC_PI_2 + angle_rad, -5.0);
+        let c = rotate_point(self.point, angle_rad, 15.0);
 
         polygon(self.color, &[a, b, c], ctx.transform, g);
     }
@@ -66,7 +70,7 @@ impl Boid {
     fn update(&mut self) -> () {
         let mut random = rand::thread_rng();
 
-        let [new_x, new_y] = rotate_point([self.x, self.y], self.direction, self.velocity);
+        let [new_x, new_y] = rotate_point(self.point, self.direction, self.velocity);
 
         if new_x < SCREEN_BUFFER {
             self.direction = random.gen_range(PI * 0.75..=PI * 1.25);
@@ -78,8 +82,7 @@ impl Boid {
             self.direction = random.gen_range(PI * 0.25..=PI * 0.75)
         }
 
-        self.x = new_x;
-        self.y = new_y;
+        self.point = Point { x: new_x, y: new_y }
     }
 }
 
